ee6473a2a7d19fe7bd2047278608090ba009b8ac
Florent <florent.nolot@univ-reims.fr>
2022-11-03 23:03:16 +0100

    Change bundle name

---
+++ RemoteLabzNetworkBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace RemoteLabz\NetworkBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RemoteLabzNetworkBundle extends Bundle
+{
+}
--- RemotelabzNetworkBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Remotelabz\NetworkBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class RemotelabzNetworkBundle extends Bundle
-{
-}
